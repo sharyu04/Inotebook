4478e2fa4b300c7470d32dede30eac6d03c9ef50
Sharyu <sharayumarwadi11@gmail.com>
2022-08-08 16:23:59 +0530

    Login done

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,7 +8,7 @@ var jwt = require('jsonwebtoken');
 const JWT_Secret = 'heyThere';
 
 
-//Create a user using: POST: "/api/auth/"
+//Create a user using: POST: "/api/auth/createUser"
 router.post('/createUser',[
     body('name').isLength({ min: 1 }),
     body('email','Enter a valid email').isEmail(),
@@ -55,5 +55,42 @@ router.post('/createUser',[
     //   res.json({error: 'Please enter a unique email', message: err.message})}
     //   )
 })
+
+//Authenticate a user using: POST: "/api/auth/login"
+router.post('/login',[
+    body('email','Enter a valid email').isEmail(),
+    body('password','Password cannot be empty').exists(),
+],async(req,res)=>{ 
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {email,password} = req.body;
+    try {
+        let user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({error: "Please try again with correct credentials"});
+        }
+
+        const passwordCompare = await bcrypt.compare(password,user.password);
+        if(!passwordCompare){
+            return res.status(400).json({error: "Please try again with correct credentials"});
+        }
+
+        const data = {
+            user: {
+                id: user.id
+            }
+          }
+          const authtoken = jwt.sign(data, JWT_Secret);
+
+        res.json({authtoken}) 
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send("Some error occured")
+    }
+} )
 
 module.exports = router
